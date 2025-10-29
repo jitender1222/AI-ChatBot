de1ef9d656427d5c3bd4750aa3fd3d0b53e6b834
jitender1222 <jeetusinghkanyal@gmail.com>
2025-10-29 22:56:07 +0530

    restructure the folders

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { askingLLMQuestion } from "../chatbot.js";
+import { askingLLMQuestion } from "./chatbot.js";
 
 const app = express();
 const port = 3000;
